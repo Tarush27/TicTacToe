601a5de1b9ff9ec6c8d4da341feafcb156505bc2
Tarush27 <tarushkaistha05@gmail.com>
2026-08-25 08:06:22 +0530

    created tic tac toe game stats

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +59,68 @@ fun GreetingPreview() {
     TicTacToeTheme {
         TicTacToeApp()
     }
+}
+
+@Composable
+fun GameStats() {
+    Surface(Modifier.fillMaxWidth()) {
+        Row(Modifier.padding(top = 25.dp),horizontalArrangement = Arrangement.SpaceEvenly) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    painterResource(R.drawable.ic_circle),
+                    contentDescription = "noughts",
+                    Modifier.size(40.dp),
+                    tint = Color.Blue
+                )
+                Spacer(Modifier.padding(top = 5.dp))
+                Text(
+                    text = "4 wins",
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    painterResource(R.drawable.ic_cross),
+                    contentDescription = "noughts",
+                    Modifier.size(40.dp),
+                    tint = Color.Blue
+                )
+                Spacer(Modifier.padding(top = 5.dp))
+                Text(
+                    text = "4 wins",
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    painterResource(R.drawable.ic_balance),
+                    contentDescription = "noughts",
+                    Modifier.size(40.dp),
+                    tint = Color.DarkGray.copy(alpha = 0.5f)
+                )
+                Spacer(Modifier.padding(top = 5.dp))
+                Text(
+                    text = "4 draws",
+                    color = Color.DarkGray.copy(alpha = 0.5f),
+                    fontSize = 20.sp,
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun GameStatsPreview() {
+
+    GameStats()
+
 }
