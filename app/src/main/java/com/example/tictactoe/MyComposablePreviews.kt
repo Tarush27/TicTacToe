e975b8159e7aedbc017e6ad019178a2ffa830d26
Tarush27 <tarushkaistha05@gmail.com>
2026-08-31 07:56:54 +0530

    created previews file for previewing composables

--- app/src/main/java/com/example/tictactoe/MyComposablePreviews.kt
+++ app/src/main/java/com/example/tictactoe/MyComposablePreviews.kt
@@ -5,7 +5,50 @@ import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
-@Preview (showBackground = true)
-fun GameBoardPreview(){
+@Preview(showBackground = true)
+fun GameBoardPreview() {
     GameBoard()
 }
+
+@Composable
+@Preview(showBackground = true)
+fun GameStatsRailPreview() {
+    GameStatsRail()
+}
+
+@Composable
+@Preview
+fun GameStatsPreview() {
+    GameStats()
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PlayerTypePreview() {
+    PlayerType()
+}
+
+@Composable
+@Preview (showBackground = true)
+fun ResetGamePreview() {
+
+    ResetGame()
+
+}
+
+@Composable
+@Preview (showBackground = true)
+fun ResetGameLandscapePreview() {
+
+    ResetGameLandscape()
+
+}
+
+@Composable
+@Preview (showBackground = true)
+fun GameBoardLandscapePreview() {
+
+    GameBoardLandscape()
+
+}
+
